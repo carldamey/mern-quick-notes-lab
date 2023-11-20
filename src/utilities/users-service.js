9647bc887a8fe2a09b86c0fd68a31a63a5f089b5
carldamey <carldamey@gmail.com>
2023-11-19 23:01:08 -0800

    begins implementation of login feature

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -2,6 +2,7 @@ import * as usersAPI from "./users-api"
 
 export async function signUp(userData) {
     const token = await usersAPI.signUp(userData)
+    localStorage.setItem("token", token)
     return token
 }
  export async function login(credentials) {
