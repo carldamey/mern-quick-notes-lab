946a900e74a3e6cf2275324d60cdea8d33dd0b2b
carldamey <carldamey@gmail.com>
2023-11-20 22:41:24 -0800

    finishes log in lab by implementing login and mutually exclusive views

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const usersCtrl = require("../../controllers/api/users")
 
 //POST /api/users
-router.post("/", usersCtrl.create)
 router.post("/login", usersCtrl.login)
+router.post("/", usersCtrl.create)
 
 module.exports = router
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,18 @@
 import SignUpForm from "../../components/SignUpForm/SignUpForm"
 import LoginForm from "../../components/LoginForm/LoginForm"
+import { useState } from "react"
 
 export default function AuthPage({setUser}) {
+	let [showLogin, setShowLogin] = useState(false)
 	return (
 		<main>
 			<h1>Auth Page</h1>
+			{showLogin ? 
 			<SignUpForm setUser={setUser} />
-			<LoginForm />
+			:
+			<LoginForm setUser={setUser}/>}
+
+		<button onClick={() => setShowLogin(!showLogin)}>{!showLogin ? "Sign Up Instead" : "Log In Instead"}</button>
 		</main>
 	)
 }
